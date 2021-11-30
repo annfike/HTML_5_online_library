71d65ae442d56d7cc89782ef6fc6b9e06816d078
Anna <44573387+annfike@users.noreply.github.com>
2021-11-30 11:51:28 +0300

    Update render_website.py

--- render_website.py
+++ render_website.py
@@ -8,14 +8,13 @@ from more_itertools import chunked
 
 
 def on_reload():
-    with open("books.json", "r") as my_file:
-        books_json = my_file.read()
-    books = json.loads(books_json)
+    with open("books.json", "r") as file:
+        books = json.load(file)
     for book in books:
         book['Обложка'] = book['Обложка'].replace('\\', '/')
         book['Ссылка'] = book['Ссылка'].replace('\\', '/')
 
-    pages = list(chunked(books, 10))
+    pages = list(chunked(books, books_on_page=10))
 
     os.makedirs('pages', exist_ok=True)
     for number, page in enumerate(pages, 1):
@@ -26,26 +25,18 @@ def on_reload():
         template = env.get_template('template.html')
 
         books_chunked = list(chunked(page, 2))
-        current_page_minus3 = number - 3
-        current_page_minus2 = number - 2
-        current_page_minus1 = number - 1
-        current_page = number
-        current_page_plus1 = number + 1
-        current_page_plus2 = number + 2
-        current_page_plus3 = number + 3
-        last_page = len(pages)
-        before_last_page = last_page - 1
+
         rendered_page = template.render(
             books=books_chunked,
-            current_page_minus3=current_page_minus3,
-            current_page_minus2=current_page_minus2,
-            current_page_minus1=current_page_minus1,
-            current_page=current_page,
-            current_page_plus1=current_page_plus1,
-            current_page_plus2=current_page_plus2,
-            last_page=last_page,
-            current_page_plus3=current_page_plus3,    
-            before_last_page=before_last_page,       
+            current_page_minus3=number - 3,
+            current_page_minus2=number - 2,
+            current_page_minus1=number - 1,
+            current_page=number,
+            current_page_plus1=number + 1,
+            current_page_plus2=number + 2,
+            last_page=len(pages),
+            current_page_plus3=number + 3,    
+            before_last_page=len(pages) - 1,       
         )
 
         with open(f'pages/index{number}.html', 'w', encoding="utf8") as file:
